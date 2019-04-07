21a03d7629c38767424e20c4dba18490000f4c50
Jani Sillanpää <jani.k.sillanpaa@tuni.fi>
2019-04-07 09:17:42 +0300

    last500fix

--- weather-app/webapp/latestData.js
+++ weather-app/webapp/latestData.js
@@ -4,18 +4,18 @@ var printLatestData = function (api) {
     $.getJSON(api, function (result) {    
       // $("#data_div").html(JSON.stringify(result));
 
-      let html = "<table class='table table-striped'><thead><tr><th>Date</th><th>Signal</th><th>Value</th></tr></thead><tbody>";
+      let html = "<table class='table table-striped'><thead><tr><th>Date</th><th>Time</th><th>Signal</th><th>Value</th></tr></thead><tbody>";
 
       for (i = 0; i < result.length; i++) {
       //  let html = "<tr>"
         let d = new Date(result[i].date_time);
         let date = d.toLocaleDateString();
         let time = d.toLocaleTimeString();
-        html += "<tr><td>" + date + " " + time + "</td><td>"
+        html += "<tr><td>" + date + "</td><td>" + time + "</td><td>"
        
 
         for (name in result[i].data) {
-            html += name + "</td><td> "  + result[i].data[name] + "</td>"
+            html += name + "</td><td>" + result[i].data[name] + "</td>"
         }
         html += "</tr>";
 
